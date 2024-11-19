d5e320f4cff2d3ea8e7891b965e9cc9c7cfaa173
TaufeqRazakh <taufeqmdr@gmail.com>
2024-11-19 03:47:23 -0500

    iterate filenames suffix with actial grid size in submit script

--- benchmark_submit.sh
+++ benchmark_submit.sh
@@ -14,18 +14,18 @@ export OMP_NUM_THREADS=1
 
 now=$(date +"%m-%d-%Y")
 
-if [[ ! -d ~/DCMESH/benchmark_results/lfd/Sunspot/${now} ]]
+if [[ ! -d ~/DCMESH_COMPUTE_CUR_M/benchmark_results/lfd/Sunspot/${now} ]]
 then
-    mkdir -p ~/DCMESH/benchmark_results/lfd/Sunspot/${now}
+    mkdir -p ~/DCMESH_COMPUTE_CUR_M/benchmark_results/lfd/Sunspot/${now}
 fi
 
-for i in {1..3}; do
-  for j in {1..3}; do
-    for k in {1..3}; do
-      for n in {1..9}; do
+for i in {32..96..32}; do
+  for j in {32..96..32}; do
+    for k in {32..96..32}; do
+      for n in {32..288..32}; do
         rm control_lfd/bench_system.in
         ln -s /home/razakh/DCMESH_COMPUTE_CUR_M/control_lfd/benchmark_system_"$i"_"$j"_"$k"_"$n".in control_lfd/bench_system.in
-        unitrace -d -o ~/DCMESH/benchmark_results/lfd/Sunspot/${now}/lfd_kernels_O${OMP_NUM_THREADS}_"$i"_"$j"_"$k"_"$n" ../../../bin/lfd -p float
+        unitrace -d -o ~/DCMESH_COMPUTE_CUR_M/benchmark_results/lfd/Sunspot/${now}/lfd_kernels_O${OMP_NUM_THREADS}_"$i"_"$j"_"$k"_"$n" ../../../bin/lfd -p float
       done
     done
   done
